7b25b4ab63f804e447f1f1f270347a59dffe528d
Feda <134657633+federalbyro@users.noreply.github.com>
2024-12-25 12:48:50 +0300

    Update forms.py

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,19 +1,45 @@
-
 from django import forms
-from .models import Comment
+from .models import Post
 
 
-class CommentForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
+    """Форма для создания и редактирования публикации."""
     class Meta:
-        model = Comment
-        fields = ['text']
+        model = Post
+        fields = ['title', 'text', 'image', 'is_published', 'pub_date', 'category', 'location']
         widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите заголовок публикации',
+            }),
             'text': forms.Textarea(attrs={
                 'class': 'form-control',
-                'rows': 3,
-                'placeholder': 'Напишите ваш комментарий здесь...'
+                'rows': 5,
+                'placeholder': 'Введите текст публикации...',
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-control-file',
+            }),
+            'is_published': forms.CheckboxInput(attrs={
+                'class': 'form-check-input',
+            }),
+            'pub_date': forms.DateTimeInput(attrs={
+                'class': 'form-control',
+                'type': 'datetime-local',
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-control',
+            }),
+            'location': forms.Select(attrs={
+                'class': 'form-control',
             }),
         }
         labels = {
-            'text': 'Комментарий',
+            'title': 'Заголовок',
+            'text': 'Текст',
+            'image': 'Изображение',
+            'is_published': 'Опубликовано',
+            'pub_date': 'Дата публикации',
+            'category': 'Категория',
+            'location': 'Местоположение',
         }
